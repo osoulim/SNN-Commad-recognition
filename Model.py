12f95132195b4239566bec63f2c696ce8c973d91
Mohammadreza Osouli <mohammadreza.osouli@gmail.com>
2020-02-05 01:23:25 +0330

    Add spikes to model

--- Model.py
+++ Model.py
@@ -17,20 +17,29 @@ class Model:
         self.layers_sizes = layers_sizes
         self.layers_num = len(layers_sizes)
         self.spikes = PriorityQueue(sum(self.layers_sizes) * 10)
-        for x in self.layers_sizes:
-            self.layers.append(Population(x))
+        for _id, x in enumerate(self.layers_sizes):
+            self.layers.append(Population(x, layer_id=_id))
         for i in range(layers_num - 1):
             for x in range(self.layers_sizes[i]):
                 for y in range(self.layers_sizes[i + 1]):
                     synapses[(x, y)] = MySynapses([self.layers[i][x]], [self.layers[i+1][y]])
     
-    def add_spike(self, layer_id, neuron_id, time):
-        self.spikes.put((time, layer_id, neuron_id))
+    def add_spike_to_input(self, neuron_id, time):
+        self.spikes.put((time, 100, 0, neuron_id))
     
-    def next_step():
+    def next_step(self):
         self.time += 1
-        while self.spikes[0][0] <= self.time:
-            spike_time, spike = self.spikes.get()
+        while len(self.spikes) > 0 and self.spikes[0][0] <= self.time:
+            spike_time, exc, layer_id, neuron_id = self.spikes.get()
             if spike_time < self.time:
                 continue
-            # TODO: spike that :DD
+            next_spikes = self.layers[layer_id][neuron_id].excite(exc)
+            for spike in next_spikes:
+                self.spikes.put(spike)
+    
+    def reset(self):
+        for layer in self.layers:
+            layer.reset()
+        for synapse in self.synapses.values():
+            synapse.reset()
+
