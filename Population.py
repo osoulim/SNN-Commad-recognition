cd185e42c1d0efaa7882989c2e8e2ac81e35c90e
Mohammadreza Osouli <mohammadreza.osouli@gmail.com>
2020-02-05 06:01:31 +0330

    Fix population bugs

--- Population.py
+++ Population.py
@@ -1,14 +1,10 @@
-from code.my_neuron import MyNeuron
-
 class Population:
-    size = 0
-    neurons = []
 
-    def __init__(self, size, neuron_class=MyNeuron, layer_id=0):
-        self.size = size
-        for i in range(self.size):
+    def __init__(self, size, layer_id, neuron_class):
+        self.neurons = list()
+        for i in range(size):
             self.neurons.append(neuron_class(neuron_id=i, layer_id=layer_id))
-    
+
     def __getitem__(self, key):
         return self.neurons[key]
 
